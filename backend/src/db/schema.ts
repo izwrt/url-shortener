6c2b84d3b56fe7bd32fb0293ed28a2b8f29918cf
izwrt <ishwart466@gmail.com>
2026-06-23 12:07:51 +0530

    wip(auth): implement singup and validation

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -2,11 +2,13 @@ import { pgTable, uuid, varchar, text, timestamp, integer } from "drizzle-orm/pg
 
 export const userTable = pgTable("users", {
     id: uuid().primaryKey().defaultRandom(),
-    name: varchar({ length:225 }).notNull(),
-    email: varchar({ length:225 }).notNull(),
+    firstName: varchar({ length:55 }).notNull(),
+    lastName: varchar({ length:55 }),
+    email: varchar({ length:225 }).notNull().unique(),
     password: text().notNull(),
     salt: text().notNull(),
-    createdAt: timestamp().defaultNow().notNull()
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()).notNull(),
 });
 
 export const urlsTable = pgTable("urls", {
