0ea6aaac4e8458e2d519179788c6f9ab5ec465dc
izwrt <ishwart466@gmail.com>
2026-07-07 21:37:21 +0530

    feat(auth): implement authentication middleware
    - add middleware to protect private routes for authorization
    - attach the decoded payload to req.user to access the user's private data
    - return respective errors for token verification failures

--- backend/src/validation/auth.validation.ts
+++ backend/src/validation/auth.validation.ts
@@ -1,38 +1,33 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-    firstName: z.string({ error: "First name is required" })
-        .trim()
-        .min(1,"First name is required")
-        .max(50, "First name cannot exceed 50 characters"),
+  firstName: z
+    .string()
+    .trim()
+    .min(1, "First name is required")
+    .max(50, "First name cannot exceed 50 characters"),
 
-    lastName: z.string()
-        .trim()
-        .max(50, "First name cannot exceed 50 characters")
-        .optional(),
+  lastName: z
+    .string()
+    .trim()
+    .max(50, "First name cannot exceed 50 characters")
+    .optional(),
 
-    email: z.string({ error: "Email is required" })
-        .trim()
-        .toLowerCase()
-        .min(1,"Email is required")
-        .max(255, "Email is too long")
-        .email(),
+  email: z
+    .string()
+    .trim()
+    .toLowerCase()
+    .min(1, { message: "Email is required" })
+    .max(255, { message: "Email is too long" })
+    .pipe(z.email({ error: "Invalid email format" })),
 
-    password: z.string({ error: "Password is required" })
-        .min(12, "Password must be at least 12 characters long")
-        .max(64, "Password cannot exceed 64 characters")
+  password: z
+    .string()
+    .min(12, "Password must be at least 12 characters long")
+    .max(64, "Password cannot exceed 64 characters"),
 });
 
 export const loginSchema = z.object({
-    email: z.string()
-        .trim()
-        .toLowerCase()
-        .min(1)
-        .max(255)
-        .email(),
-
-    password: z.string()
-    .min(1)
+  email: z.string().trim().toLowerCase().min(1).max(255).pipe(z.email()),
+  password: z.string().min(1),
 });
-
-
